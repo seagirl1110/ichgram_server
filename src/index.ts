79affd5b18898c7383f7a97e4118b997fc036338
Marina Elizarova <seagirl1110@gmail.com>
2025-01-09 13:33:22 +0300

    feat: add setups

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/db';
 import authRoutes from './routes/auth';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 
